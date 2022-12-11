5afebffb2ade2a3e1f8a8abed063d044f762863d
Simon Bolt <sibolt@student.ethz.ch>
2022-12-12 00:05:59 +0100

    change array sizes, BC

--- scripts/StokesSolver_prototype.jl
+++ scripts/StokesSolver_prototype.jl
@@ -8,16 +8,15 @@ This script solves the 2D Stokes & Continuity equations for given arrays of μ, 
 τyy = 2μ*∂vy/∂y
 τxy = τyx = μ*(∂vx/∂y + ∂vy/∂x)
 
-PT iterations are implemented as in the 2D minapp of ParallelStencil.jl
+PT iterations are implemented similarly to the 2D minapp of ParallelStencil.jl
 https://github.com/omlins/ParallelStencil.jl/blob/main/miniapps/Stokes2D.jl
 =#
 
 #=
 What's still missing:
-- Free slip BC and correct array sizes for BC implementation
 - Performance (Kernel fusing, tuning relaxation parameters, ...)
-- Adapting the equations to avoid extremely large visosities, maybe pressures
-- Also use viscosity on τxy-nodes, i.e. μ_τxy instead of averaging of pressure nodes μ_p
+  see e.g. https://github.com/PTsolvers/PseudoTransientStokes.jl/blob/main/scripts/Stokes2D_ve_perf.jl
+- Handle the units more elegantly (non-dimensional, just omit?)
 =#
 
 const USE_GPU = true
@@ -33,16 +32,13 @@ using Plots, Printf, Statistics
 
 @views function Stokes2D()
     # Physics
-    lx, ly = 100000, 100000 # domain size
-    μ_air, μ_matrix, μ_plume = 1e17, 1e19, 1e18 # Viscosity TODO units
-    ρ_air, ρ_matrix, ρ_plume = 1   , 3300, 3200 # Density TODO units
+    lx, ly = [100000, 100000]./100000 # domain size
+    μ_air, μ_matrix, μ_plume = [1e17, 1e19, 1e18]./1e19 # Viscosity
+    ρ_air, ρ_matrix, ρ_plume = [1   , 3300, 3200]./1000 # Density
     g_y = 9.81
     plume_x, plume_y = lx/2, ly/2 # plume midpoint
-    plume_r = 20000 # plume radius
+    plume_r = 20000 / 100000 # plume radius
     air_height = 0.2*ly
-
-    #μ_air = μ_matrix
-    #ρ_air = ρ_matrix
 
     # Numerics
     nx, ny = 127,127#35, 45
@@ -51,25 +47,29 @@ using Plots, Printf, Statistics
     dx, dy = lx/(nx-1), ly/(ny-1)
 
     # Array allocations
-    P    = @zeros(nx  ,ny  )
-    τxx  = @zeros(nx  ,ny  )
-    τyy  = @zeros(nx  ,ny  )
-    Vx   = @zeros(nx+1,ny  )
-    Vy   = @zeros(nx  ,ny+1)
-    τxy  = @zeros(nx-1,ny-1)
+    P    = @zeros(nx-1,ny-1)
+    τxx  = @zeros(nx-1,ny-1) #p-nodes
+    τyy  = @zeros(nx-1,ny-1) #p-nodes
+    Vx   = @zeros(nx  ,ny+1)
+    Vy   = @zeros(nx+1,ny  )
+    τxy  = @zeros(nx  ,ny  )
 
     # coordinates for points
-    x_p  = [(ix-1)*dx       for ix=1:nx  ]
-    y_p  = [(iy-1)*dy       for iy=1:ny  ]
-    x_vy = [(ix-1)*dx       for ix=1:nx  ]
-    y_vy = [(iy-1)*dy-0.5dy for iy=1:ny+1]
-    x_τxy= [(ix-1)*dx+0.5dx for ix=1:nx-1]
-    y_τxy= [(iy-1)*dy+0.5dy for iy=1:ny-1]
+    x    = [(ix-1)*dx       for ix=1:nx  ] # basic nodes = τ_xy-nodes
+    y    = [(iy-1)*dy       for iy=1:ny  ]
+    x_p  = [(ix-1)*dx+0.5dx for ix=1:nx-1] # pressure nodes
+    y_p  = [(iy-1)*dy+0.5dy for iy=1:ny-1]
+    x_vx = x                               # Vx nodes
+    y_vx = [(iy-1)*dy-0.5dy for iy=1:ny+1]
+    x_vy = [(ix-1)*dx-0.5dx for ix=1:nx+1] # Vy nodes
+    y_vy = y
+    x_τxy= x
+    y_τxy= y
 
     # set density & viscosity
-    ρ_vy  = zeros(nx  ,ny+1) # on Vy-nodes
-    μ_τxy = zeros(nx-1,ny-1) # on τxy-nodes
-    μ_p   = zeros(nx  ,ny  ) # on P-nodes (also τxx-, τyy-nodes)
+    ρ_vy  = zeros(nx+1,ny  ) # on Vy-nodes
+    μ_τxy = zeros(nx  ,ny  ) # on τxy-nodes
+    μ_p   = zeros(nx-1,ny-1) # on P-nodes (also τxx-, τyy-nodes)
     set_properties!(x_p  ,y_p  ,μ_p  ,plume_x,plume_y,plume_r,μ_matrix,μ_plume,μ_air,air_height) # viscosity: P-nodes
     set_properties!(x_τxy,y_τxy,μ_τxy,plume_x,plume_y,plume_r,μ_matrix,μ_plume,μ_air,air_height) # viscosity: τxy-nodes
     set_properties!(x_vy ,y_vy ,ρ_vy ,plume_x,plume_y,plume_r,ρ_matrix,ρ_plume,ρ_air,air_height) # density: Vy-nodes
@@ -86,15 +86,14 @@ using Plots, Printf, Statistics
 
 
     # more arrays
-    ∇V        = @zeros(nx  ,ny  )
-    dτPt      = @zeros(nx  ,ny  )
-    Rx        = @zeros(nx-1,ny-2)
-    Ry        = @zeros(nx-2,ny-1)
-    dVxdτ     = @zeros(nx-1,ny-2)
-    dVydτ     = @zeros(nx-2,ny-1)
-    dτVx      = @zeros(nx-1,ny-2)
-    dτVy      = @zeros(nx-2,ny-1)
-    Rog = ρ_vy[:,2:end]*g_y # hack
+    ∇V        = @zeros(nx-1,ny-1)
+    dτPt      = @zeros(nx-1,ny-1)
+    Rx        = @zeros(nx-2,ny-1)
+    Ry        = @zeros(nx-1,ny-2)
+    dVxdτ     = @zeros(nx-2,ny-1)
+    dVydτ     = @zeros(nx-1,ny-2)
+    dτVx      = @zeros(nx-2,ny-1)
+    dτVy      = @zeros(nx-1,ny-2)
     Vdmp      = 4.0
     Vsc       = 0.25#1.0         # relaxation paramter for the momentum equations pseudo-timesteps limiters
     Ptsc      = 1.0/4.0 
@@ -106,7 +105,7 @@ using Plots, Printf, Statistics
 
     # PT setup and iterations
     @parallel compute_timesteps!(dτVx, dτVy, dτPt, μ_p, Vsc, Ptsc, min_dxy2, max_nxy)
-    ncheck = 20
+    ncheck = 500
     t1 = 0; itert1 = 11
     ϵ = 0.01 # tol
     err_evo1=[]; err_evo2=[]
@@ -114,16 +113,18 @@ using Plots, Printf, Statistics
     while err > ϵ && iter <= iterMax
         if (iter==itert1) t1 = Base.time() end
 
-        # TODO use μ_τxy where appropriate
         # TODO improve kernels
         # TODO make var names consistent
         @parallel compute_P!(∇V, P, Vx, Vy, dτPt, dx, dy)
-        @parallel compute_τ!(∇V, τxx, τyy, τxy, Vx, Vy, μ_p, dx, dy)
-        @parallel compute_dV!(Rx, Ry, dVxdτ, dVydτ, P, Rog, τxx, τyy, τxy, dampX, dampY, dx, dy)
+        @parallel compute_τ!(∇V, τxx, τyy, τxy, Vx, Vy, μ_p, μ_τxy, dx, dy)
+        @parallel compute_dV!(Rx, Ry, dVxdτ, dVydτ, P, ρ_vy, τxx, τyy, τxy, g_y, dampX, dampY, dx, dy)
         @parallel compute_V!(Vx, Vy, dVxdτ, dVydτ, dτVx, dτVy)
 
-        @parallel (1:size(Vx,1), 1:size(Vx,2)) bc_y!(Vx)
-        @parallel (1:size(Vy,1), 1:size(Vy,2)) bc_x!(Vy)
+        # Free slip BC
+        @parallel (1:size(Vx,1)) bc_y_noflux!(Vx)
+        @parallel (1:size(Vx,2)) bc_x_zero!(Vx)
+        @parallel (1:size(Vy,1)) bc_y_zero!(Vy)
+        @parallel (1:size(Vy,2)) bc_x_noflux!(Vy)
 
         if iter%ncheck == 0
             # TODO compute error better
@@ -147,54 +148,67 @@ using Plots, Printf, Statistics
     p2 = heatmap(x_vy, y_vy, Array(Vy)', yflip=true, aspect_ratio=1, xlims=(0,lx), ylims=(0,ly), c=:inferno, title="Vy")
     #p4 = heatmap(X[2:end-1], Yv[2:end-1], log10.(abs.(Array(Ry)')), aspect_ratio=1, xlims=(X[2],X[end-1]), ylims=(Yv[2],Yv[end-1]), c=:inferno, title="log10(Ry)")
     p5 = plot(err_evo2,err_evo1, legend=false, xlabel="# iterations", ylabel="log10(error)", linewidth=2, markershape=:circle, markersize=3, labels="max(error)", yaxis=:log10)
+    p6 = heatmap(x_vx, y_vx, Array(Vx)', yflip=true, aspect_ratio=1, xlims=(0,lx), ylims=(0,ly), c=:inferno, title="Vx")
     #display(plot(p1, p2, p4, p5))
-    display(plot(p1,p2,p5))
+    display(plot(p1,p2,p5,p6))
 
     return nothing
 end
 
 
-@parallel function compute_timesteps!(dτVx::Data.Array, dτVy::Data.Array, dτPt::Data.Array, Mus::Data.Array, Vsc::Data.Number, Ptsc::Data.Number, min_dxy2::Data.Number, max_nxy::Int)
-    @all(dτVx) = Vsc*min_dxy2/@av_xi(Mus)/4.1
-    @all(dτVy) = Vsc*min_dxy2/@av_yi(Mus)/4.1
-    @all(dτPt) = Ptsc*4.1*@all(Mus)/max_nxy
+@parallel function compute_timesteps!(dτVx, dτVy, dτPt, μ_p, Vsc, Ptsc, min_dxy2, max_nxy)
+    @all(dτVx) = Vsc*min_dxy2/@av_xa(μ_p)/4.1
+    @all(dτVy) = Vsc*min_dxy2/@av_ya(μ_p)/4.1
+    @all(dτPt) = Ptsc*4.1*@all(μ_p)/max_nxy
     return
 end
 
-@parallel function compute_P!(∇V::Data.Array, Pt::Data.Array, Vx::Data.Array, Vy::Data.Array, dτPt::Data.Array, dx::Data.Number, dy::Data.Number)
-    @all(∇V)  = @d_xa(Vx)/dx + @d_ya(Vy)/dy
+@parallel function compute_P!(∇V, Pt, Vx, Vy, dτPt, dx, dy)
+    @all(∇V)  = @d_xi(Vx)/dx + @d_yi(Vy)/dy
     @all(Pt)  = @all(Pt) - @all(dτPt)*@all(∇V)
     return
 end
 
-@parallel function compute_τ!(∇V::Data.Array, τxx::Data.Array, τyy::Data.Array, τxy::Data.Array, Vx::Data.Array, Vy::Data.Array, Mus::Data.Array, dx::Data.Number, dy::Data.Number)
-    @all(τxx) = 2.0*@all(Mus)*(@d_xa(Vx)/dx - 1.0/3.0*@all(∇V))
-    @all(τyy) = 2.0*@all(Mus)*(@d_ya(Vy)/dy - 1.0/3.0*@all(∇V))
-    @all(τxy) = 2.0*@av(Mus)*(0.5*(@d_yi(Vx)/dy + @d_xi(Vy)/dx))
+@parallel function compute_τ!(∇V, τxx, τyy, τxy, Vx, Vy, μ_p, μ_τxy, dx, dy)
+    @all(τxx) = 2.0*@all(μ_p)*(@d_xi(Vx)/dx - 1.0/3.0*@all(∇V))
+    @all(τyy) = 2.0*@all(μ_p)*(@d_yi(Vy)/dy - 1.0/3.0*@all(∇V))
+    @all(τxy) = 2.0*@all(μ_τxy)*(0.5*(@d_ya(Vx)/dy + @d_xa(Vy)/dx))
     return
 end
 
-@parallel function compute_dV!(Rx::Data.Array, Ry::Data.Array, dVxdτ::Data.Array, dVydτ::Data.Array, Pt::Data.Array, Rog::Data.Array, τxx::Data.Array, τyy::Data.Array, τxy::Data.Array, dampX::Data.Number, dampY::Data.Number, dx::Data.Number, dy::Data.Number)
-    @all(Rx)    = @d_xi(τxx)/dx + @d_ya(τxy)/dy - @d_xi(Pt)/dx
-    @all(Ry)    = @d_yi(τyy)/dy + @d_xa(τxy)/dx - @d_yi(Pt)/dy + @av_yi(Rog)
+@parallel function compute_dV!(Rx, Ry, dVxdτ, dVydτ, Pt, ρ_vy, τxx, τyy, τxy, g_y, dampX, dampY, dx, dy)
+    @all(Rx)    = @d_xa(τxx)/dx + @d_yi(τxy)/dy - @d_xa(Pt)/dx
+    @all(Ry)    = @d_ya(τyy)/dy + @d_xi(τxy)/dx - @d_ya(Pt)/dy + @inn(ρ_vy)*g_y
     @all(dVxdτ) = dampX*@all(dVxdτ) + @all(Rx)
     @all(dVydτ) = dampY*@all(dVydτ) + @all(Ry)
     return
 end
 
-@parallel function compute_V!(Vx::Data.Array, Vy::Data.Array, dVxdτ::Data.Array, dVydτ::Data.Array, dτVx::Data.Array, dτVy::Data.Array)
+@parallel function compute_V!(Vx, Vy, dVxdτ, dVydτ, dτVx, dτVy)
     @inn(Vx) = @inn(Vx) + @all(dτVx)*@all(dVxdτ)
     @inn(Vy) = @inn(Vy) + @all(dτVy)*@all(dVydτ)
     return
 end
 
-@parallel_indices (ix,iy) function bc_x!(A::Data.Array)
+# side boundaries
+@parallel_indices (iy) function bc_x_zero!(A::Data.Array)
+    A[1  , iy] = 0.0
+    A[end, iy] = 0.0
+    return
+end
+@parallel_indices (iy) function bc_x_noflux!(A::Data.Array)
     A[1  , iy] = A[2    , iy]
     A[end, iy] = A[end-1, iy]
     return
 end
 
-@parallel_indices (ix,iy) function bc_y!(A::Data.Array)
+# horizontal boundaries
+@parallel_indices (ix) function bc_y_zero!(A::Data.Array)
+    A[ix, 1  ] = 0.0
+    A[ix, end] = 0.0
+    return
+end
+@parallel_indices (ix) function bc_y_noflux!(A::Data.Array)
     A[ix, 1  ] = A[ix, 2    ]
     A[ix, end] = A[ix, end-1]
     return
