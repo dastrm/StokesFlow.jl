07cf360a1bf7f602d32856d2208d1b53100fdfa4
Simon Bolt <sibolt@student.ethz.ch>
2023-01-14 00:21:49 +0100

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,19 +7,19 @@ testdir = @__DIR__
 istest(f) = endswith(f, ".jl") && startswith(f, "test_")
 testfiles = sort(filter(istest, readdir(testdir)))
 
-nprocs = 4
+nprocs_default = 4
 USE_GPU = false
 
 @testset "$f" for f in testfiles
     MPI.mpiexec() do mpirun
-        cmd(n=nprocs) = `$mpirun -n $n $(Base.julia_cmd()) $(joinpath(testdir, f))`
+        cmd(n=nprocs_default) = `$mpirun -n $n $(Base.julia_cmd()) $(joinpath(testdir, f))`
 
         withenv("USE_GPU" => USE_GPU) do
 
             if f == "test_StokesSolver.jl"
                 # run this test with up to 4 processes
-                # TODO: this test passes, except when BOTH --check-bounds=true and USE_GPU==true (however no bounds-check fails...)
-                for npr = 1:min(nprocs, 4)
+                # this test passes, except when BOTH --check-bounds=true and USE_GPU==true (however no bounds-check fails...)
+                for npr = 1:4
                     r = run(ignorestatus(cmd(npr)))
                     @test success(r)
                 end
@@ -31,6 +31,12 @@ USE_GPU = false
                 # run this test with exactly 9 processes
                 r = run(ignorestatus(cmd(9)))
                 @test success(r)
+            elseif f == "test_MarkerExchange_varP.jl"
+                # run this test with up to 9 processors
+                for npr = [1,2,3,4,6,8,9]
+                    r = run(ignorestatus(cmd(npr)))
+                    @test success(r)
+                end
             else
                 r = run(ignorestatus(cmd()))
                 @test success(r)
