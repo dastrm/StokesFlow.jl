97879b72fed084953d1dfddd0f32ea156d43db93
Simon Bolt <sibolt@student.ethz.ch>
2023-01-13 04:13:52 +0100

    add TODO tests

---
+++ test/test_MarkerExchange.jl
@@ -0,0 +1,7 @@
+#=
+TODO
+
+test marker exchange, see if as expected.
+test also for only 1 processor
+
+=#
---
+++ test/test_StokesFlow2D.jl
@@ -0,0 +1,8 @@
+#=
+TODO
+
+call StokesFlow2D_multixpu with 1 and 2x2 processes with identical global domain. compare both to the same reference.
+
+(maybe call StokesFlow2D_gpu / cpu as well, compare to the same reference)
+
+=#
